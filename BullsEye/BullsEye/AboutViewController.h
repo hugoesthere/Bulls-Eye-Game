d6c32d7aaca04fdd50d1827f95b6c6dfe7ac3e9f
Hugo <hbh02012@pomona.edu>
2014-09-06 13:09:34 -0700

    added info view control

---
+++ BullsEye/BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Hugo Ho on 9/6/14.
+//  Copyright (c) 2014 Hugo Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
